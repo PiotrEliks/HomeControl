e73f13ce4c6315aef6d939454ed4e19569e064a2
PiotrEliks <piotr.eliks@wp.pl>
2025-03-27 13:22:33 +0100

    Deploy setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {  
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
+
 let connectedDevice = null;
 let ledState = false;
 
